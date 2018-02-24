18e74d98e969445792b6c1cd13d476ab8afd18d7
Juanfran Benito <jbc0056@alu.ubu.es>
2018-02-24 21:50:48 +0100

    testAcquireReusable() realizado.
    Completado el segundo test, añadidas nuevas variables e indentación del código.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.NotFreeInstanceException;
 import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
@@ -17,15 +18,16 @@ import ubu.gii.dass.c01.ReusablePool;
  *
  */
 public class ReusablePoolTest {
-	
+
 	ReusablePool pool;
+	Reusable r1, r2, r3;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		pool=ReusablePool.getInstance();
+		pool = ReusablePool.getInstance();
 	}
 
 	/**
@@ -33,30 +35,42 @@ public class ReusablePoolTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
-		pool=null;
+		pool = null;
+		r1 = null;
+		r2 = null;
+		r3 = null;
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
-	 * @throws Exception 
 	 */
 	@Test
-	public void testGetInstance() throws Exception {
-		setUp();
+	public void testGetInstance() {
 		assertEquals(pool, ReusablePool.getInstance());
-		tearDown();
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
 	@Test
-	public void testAcquireReusable() {
-		fail("Not yet implemented");
+	public void testAcquireReusable() throws Exception {
+		r1 = pool.acquireReusable();
+		r2 = pool.acquireReusable();
+		try {
+			r3 = pool.acquireReusable();
+			assertEquals(r3, null);
+		} catch (NotFreeInstanceException e) {
+
+		}
+		pool.releaseReusable(r1);
+		pool.releaseReusable(r2);
+		assertEquals(r2, pool.acquireReusable());
+		assertEquals(r1, pool.acquireReusable());
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
 	 */
 	@Test
 	public void testReleaseReusable() {
